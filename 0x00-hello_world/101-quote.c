601b0fd11fe4782d2fe233a103b056e276a64514
Daniman-1 <danayarefayne@gmail.com>
2022-12-08 13:04:53 -0800

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
-#include <stdio.h> 
-#include <unistd.h> 
- /** 
-  *main - Entry point 
-  * 
-  *Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19' 
-  *
-  * Return: Always 0 (success) 
-  */ 
-  
-int main(void) 
-{ 
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
-	return (1); 
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
 }
