7266b2567dabb717d3a3353f0a9cc40c0c16e1d7
Daniman-1 <danayarefayne@gmail.com>
2022-12-09 15:09:31 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - Enter points
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c ='a';
+	while 
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'z') {
+			putchar(c);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
