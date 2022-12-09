242a93ccf292a2faeeada34d459cea3f49dae1d2
Daniman-1 <danayarefayne@gmail.com>
2022-12-09 14:52:12 -0800

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,20 +1,20 @@
-#include <stdlib.h>                                                                                                                 
-#include <time.h>                                                                                                                   
-#include <stdio.h>                                                                                                                  
-/* more headers goes there */                                                                                                       
-                                                                                                                                    
-/* betty style doc for function main goes there */                                                                                  
-/**                                                                                                                                 
- *main - Entry point                                                                                                                
- *                                                                                                                                  
- *Return: Always 0 (success)                                                                                                        
- */                                                                                                                                 
-int main(void)
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point 
+ *
+ * Return Always 0 (success)
+ */
+int main(void)
 {
-	int n;
+	int n;
 
 	int m;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
@@ -25,5 +25,5 @@ int main(void)
 		printf("if the last digit of %d is %d and is 0\n", n, m);
 	if(m < 6 && m != 0)
 		printf("if the last digit of %d is %d and is less than 6 and not 0\n", n, m);
-	return (0);	    
+	return (0);
 }
