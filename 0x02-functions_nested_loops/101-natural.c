318aab0c15953e76e6a6ae92bc19e9cefbe4529c
Daniman-1 <danayarefayne@gmail.com>
2022-12-13 15:30:48 -0800

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,18 +4,18 @@
   * @void: void
   * Return: 0 Success
   **/
-int main(void)
+int main(void)
 {
 	int n;
 	int sum = 0;
 
-	for (n = 0; n < 1024; n++)
+	for (n = 0; n < 1024; n++)
 	{
-		if (n % 3 == 0 || n % 5 == 0)
-		{	
-			sum += n;
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			sum +=n;
 		}
 	}
-	printf("%d\n", sum);
-	return (0);
+	printf("%d\n", sum);
+	return (0);
 }
